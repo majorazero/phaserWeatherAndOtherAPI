3a32d0059204e3560dadd77826cb60a79d1b5063
majorazero <daniel.cw.hsu@gmail.com>
2018-02-27 17:39:46 -0800

    added the node weatherapp api I'm going to play with

---
+++ javascript/weatherCall.js
@@ -0,0 +1,18 @@
+let request = require("request");
+
+//OpenWeatherMap API
+let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
+let city = "London";
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+
+request(url, function(err,res,body){
+  if(err){
+    console.log('error:',err);
+  }
+  else{
+    let weather = JSON.parse(body);
+    let message = `${weather.name}'s Temperature:${weather.main.temp} Degrees`;
+    console.log(message);
+  }
+});
